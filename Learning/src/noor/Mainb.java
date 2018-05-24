23a1e9066c08f4cb7adb2c101809b0c941e60b44
Noor-Ain <39127462+Noor-Ain@users.noreply.github.com>
2018-05-24 13:00:50 +0200

    Add files via upload

---
+++ Learning/src/noor/Mainb.java
@@ -0,0 +1,28 @@
+package noor;
+
+public class Mainb {
+
+        public int getNumber() {
+                return number;
+        }
+
+        public void setNumber(int number) {
+                this.number = number;
+        }
+
+        int number;
+        static String numberToString(int[] input) {
+                StringBuffer output = new StringBuffer(" ");
+                for (int value : input) {
+                        output.append(value);
+                }
+                return output.toString();
+        }
+
+        public static void main(String args[]) {
+                int [] inputArray = {1,2,3,4,5};
+                System.out.println(numberToString(inputArray));
+        }
+}
+
+
